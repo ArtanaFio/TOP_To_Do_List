637185ee795aa9f0a8b537adb77164de60762f93
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-08-09 18:02:40 -0400

    Update modules to make and edit items and projects
    Edited the todo-item.js and projects.js with new methods to create, and
    edit properties. Created methods to remove individual notes and
    checklist items from any todo item. Updated the dueDate property to
    recognize and accept specific date and time format. Used static
    priorities property to ensure users only use those specific options.
    Made external functions allow users to create and remove unique labels
    according to their preference if they did not want to use predetermined
    labels.

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,16 +1,107 @@
 // Project Management Module
 // single responsibility purpose: manage projects that group todo-items
 
+
+const labels = new Set(["Work", "School", "Groceries", "Goals", "Daily", "Weekly", "Monthly", "Yearly"]);
+let userUniqueLabels = [];
+
+//function to let users customize labels
+function makeLabels(label) {
+    if (!labels.has(label) || !userUniqueLabels.includes(label)) {
+        userUniqueLabels.push(label);
+        console.log(`${label} label has been created.`);
+    } else {
+        console.log(`Error: ${label} label already exists.`)
+    }
+}
+
+function deleteLabels(label) {
+    const index = userUniqueLabels.indexOf(label);
+    if (index > -1) {
+        userUniqueLabels.splice(index, 1);
+        console.log(`${label} label has been deleted.`);
+    } else {
+        console.log(`Error: ${label} label does not exist.`);
+    }
+}
+
 // create project using a class
-class Project {
-    constructor(title) {
+class makeProject {
+
+    static PRIORITIES = new Set(["low priority", "average priority", "high priority"]);
+
+    constructor(title, description, dueDate, priority, label) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
+        this.dueDate = dueDate ? new Date(dueDate) : null;
+        this.priority = this.setPriority(priority);
         this.label = label;
     }
+
+    // method to edit project title
+    editTitle(newTitle) {
+        if (newTitle) {
+            this.title = newTitle;
+        } else {
+            console.log("Error: title is empty.");
+        }
+    }
+
+    // method to edit project description
+    editDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    // method to edit project due date
+    editDueDate(newDueDate) {
+        const date = new Date(newDueDate);
+        if (!isNaN(date.getTime())) {
+            this.dueDate = date;
+        } else {
+            console.log("Error: date format is invalid.")
+        }
+    }
+
+    // method to set priority
+    setPriority(priority) {
+        if (makeProject.PRIORITIES.has(priority)) {
+            return priority;
+        } else {
+            console.log("Error: Invalid priority. Choose from the appropriate list of priorities.");
+        }
+    }
+
+    // method to edit project priority
+    editPriority(newPriority) {
+        if (makeProject.PRIORITIES.has(newPriority)) {
+            this.priority = newPriority;
+        } else {
+            console.log("Error: Invalid priority. Choose from the appropriate list of priorities.");
+        }
+    }
+
+    // method to set label
+    setLabel(label) {
+        if (labels.has(label) || userUniqueLabels.includes(label)) {
+            this.label.add(label);
+        } else {
+            console.log("Error: the label is invalid.")
+        }
+    }
+
+    //method to remove label 
+    removelabel(label) {
+        if(this.label.has(label)) {
+            this.label.delete(label);
+            console.log(`${label} label has been removed from ${this.title}`);
+        } else {
+            console.log(`Error: ${label} label was not assigned to ${this.title}`);
+        }
+    }
 };
+
+// creation of a default project
+//const defaultProject = new makeProject("Default List");
 
 // edit project
 
--- src/modules/todo-items.js
+++ src/modules/todo-items.js
@@ -1,24 +1,117 @@
+
 // create todo-item
 export class makeTodoItem {
+    static PRIORITIES = new Set(["low priority", "average priority", "high priority"]);
+
     constructor(title, description, dueDate, priority, notes, checklist) {
         this.title = title; // string
         this.description = description; // string
-        this.dueDate = dueDate; // string?
-        this.priority = priority; // string
+        this.dueDate = dueDate ? new Date(dueDate) : null; // date object
+        this.priority = this.setPriority(priority); // priority object
         this.notes = notes; // array of strings
         this.checklist = checklist; // array of strings
     }
 
-    addNotes(note) {
-        makeTodoItem.notes.push(note);
+    // method to edit todo-item title
+    editTitle(newTitle) {
+        if (newTitle) {
+            this.title = newTitle;
+        } else {
+            console.log("Error: title is empty.");
+        }
     }
 
-    addChecklist(step) {
-        makeTodoItem.checklist.push(step);
+    // method to edit todo-item description
+    editDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    // method to edit todo-item due date
+    editDueDate(newDueDate) {
+        const date = new Date(newDueDate);
+        if (!isNaN(date.getTime())) {
+            this.dueDate = date;
+        } else {
+            console.log("Error: date format is invalid.")
+        }
+    }
+
+    // method to set priority
+    setPriority(priority) {
+        if (makeTodoItem.PRIORITIES.has(priority)) {
+            return priority;
+        } else {
+            console.log("Error: Invalid priority. Choose from the appropriate list of priorities.");
+        }
+    }
+
+    // method to edit todo-item priority
+    editPriority(newPriority) {
+        if (makeTodoItem.PRIORITIES.has(newPriority)) {
+            this.priority = newPriority;
+        } else {
+            console.log("Error: Invalid priority. Choose from the appropriate list of priorities.");
+        }
+    }
+
+    // method to add todo-item note
+    addNote(note) {
+        this.notes.push(note);
+    }
+
+    // method to edit todo-item note depending on index
+    editNote(index, newNote) {
+        if (this.notes[index]) {
+            this.notes[index] = newNote;
+        } else {
+            console.log("Error: note index is invalid.");
+        }
+    }
+
+    // method to delete todo-item note depending on index
+    deleteNote(index) {
+        if(this.notes[index]) {
+            this.notes.splice(index, 1);
+        } else {
+            console.log("Error: note cannot be deleted because index is invalid.");
+        }
+    }
+
+    // method to add to todo-item's checklist
+    addChecklistItem(item) {
+        this.checklist.push({ item, completed: false });
+    }
+
+    // method to edit todo-item's checklist depending on index
+    editChecklistItem(index, newItem) {
+        if (this.checklist[index]) {
+            this.checklist[index].item = newItem;
+        } else {
+            console.log("Error: checklist index is invalid.");
+        }
+    }
+
+    // method to delete todo-item's checklist item depending on index
+    deleteChecklistItem(index) {
+        if (this.checklist[index]) {
+            this.checklist.splice(index, 1);
+        } else {
+            console.log("Error: checklist item cannot be deleted because index is invalid.");
+        }
+    }
+
+    // method to edit completion status of todo-item's checklist item
+    toggleChecklistStatus(index) {
+        if (this.checklist[index]) {
+            this.checklist[index].completed = !this.addChecklistItem.completed;
+        } else {
+            console.log("Error: Checklist item index is invalid.");
+        }
     }
 }
 
-// edit todo-item
+// add todo-items to projects
+// project.todoItems.push(todoItems);
 
 
 // delete todo-item
